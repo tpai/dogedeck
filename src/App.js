e56268b02d3677631fc9f4049a1f7016ab3cc6d8
Tony Pai <tony77794@gmail.com>
2021-10-12 23:23:07 +0800

    Prettify App.js

--- src/App.js
+++ src/App.js
@@ -12,31 +12,39 @@ function App() {
   const handleSearchChange = useCallback((e) => {
     setSearch(e.target.value);
   }, []);
-  const displayCards =cards
-  .reverse()
-  .filter(
-    (card) =>
-      card.title.includes(search) && (card.type === type || type === "")
-  )
-  .map((card) => (
-    <img
-      key={encodeURIComponent(card.title)}
-      className="object-cover object-center w-1/2 rounded-lg sm:w-1/3 lg:w-1/6"
-      alt={card.title}
-      src={card.src}
-    />
-  ))
+  const displayCards = cards
+    .reverse()
+    .filter(
+      (card) =>
+        card.title.includes(search) && (card.type === type || type === "")
+    )
+    .map((card) => (
+      <img
+        key={encodeURIComponent(card.title)}
+        className="object-cover object-center w-1/2 rounded-lg sm:w-1/3 lg:w-1/6"
+        alt={card.title}
+        src={card.src}
+      />
+    ));
   return (
     <section className="text-gray-400 bg-gray-900 body-font">
       <header className="flex p-3">
         <select
           onChange={handleTypeChange}
-          className="w-2/6 py-2 pl-3 text-white bg-transparent border border-gray-700 rounded appearance-none sm:w-24 cursor-pointer"
+          className="w-2/6 py-2 pl-3 text-white bg-transparent border border-gray-700 rounded appearance-none cursor-pointer sm:w-24"
         >
-          <option value="" className="bg-gray-900">全部</option>
-          <option value="trap" className="bg-gray-900">陷阱卡</option>
-          <option value="magic" className="bg-gray-900">魔法卡</option>
-          <option value="monster" className="bg-gray-900">怪獸卡</option>
+          <option value="" className="bg-gray-900">
+            全部
+          </option>
+          <option value="trap" className="bg-gray-900">
+            陷阱卡
+          </option>
+          <option value="magic" className="bg-gray-900">
+            魔法卡
+          </option>
+          <option value="monster" className="bg-gray-900">
+            怪獸卡
+          </option>
         </select>
         <input
           type="text"
@@ -46,7 +54,12 @@ function App() {
           className="w-3/6 px-3 py-1 ml-3 text-base text-gray-100 bg-gray-600 border border-gray-600 rounded outline-none sm:w-80 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 focus:border-indigo-500 leading-8 transition-colors duration-200 ease-in-out"
           onChange={handleSearchChange}
         />
-        <a className="flex items-center justify-center w-1/6 ml-3 sm:w-12" href="https://github.com/tpai/dogedeck" target="_blank" rel="noreferrer">
+        <a
+          className="flex items-center justify-center w-1/6 ml-3 sm:w-12"
+          href="https://github.com/tpai/dogedeck"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img
             src={githubSVG}
             alt="github"
@@ -59,7 +72,9 @@ function App() {
           {displayCards}
         </div>
       ) : (
-        <div className="w-full text-center text-xl">找不到你想要的色色柴犬🥲</div>
+        <div className="w-full text-xl text-center">
+          找不到你想要的色色柴犬🥲
+        </div>
       )}
     </section>
   );
