90e4fc0d1e4dba13b16c485888bf5e18f067d938
Tony Pai <tony77794@gmail.com>
2022-06-18 00:04:15 +0800

    Update backup script

--- src/utils/backup.js
+++ src/utils/backup.js
@@ -9,19 +9,17 @@ const toDataURL = async (url) => {
   return `data:${contentType};base64,` + buffer.toString("base64");
 };
 
+const backupCards = fs.readdirSync('./cards')
+
 Promise.all(
   cards.map(async (card) => {
+    if (card.src.slice(-3) === "gif") return;
+    if (backupCards.find(backupCard => backupCard.includes(card.title))) return;
+
     const dataUrl = await toDataURL(card.src);
-    return {
-      title: card.title,
-      type: card.type,
-      src: dataUrl,
-    };
-  })
-).then((cards) => {
-  cards.map((card) => {
-    const data = card.src.replace(/^data:image\/\w+;base64,/, "");
+    console.log(card.title, card.src);
+    const data = dataUrl.replace(/^data:image\/\w+;base64,/, "");
     const buf = Buffer.from(data, "base64");
     fs.writeFileSync(`./cards/${card.title.replace(/[":<>|*?]+/, '')}.png`, buf);
-  });
-});
+  })
+);
